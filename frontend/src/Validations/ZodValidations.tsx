46a4ac2b8cd7dd1f2305cfb7dde820e44a79944d
bhavesh joshi <joshbhavesh10@gmail.com>
2026-05-11 17:47:34 +0530

    Solved Inconsistency of passwords in frontend and backend

--- frontend/src/Validations/ZodValidations.tsx
+++ frontend/src/Validations/ZodValidations.tsx
@@ -2,6 +2,6 @@ import { z } from "zod"
 
 export const Validations = z.object({
    email : z.string().includes("@") ,
-   Password : z.string().min(10).regex(/[0-9]/) 
+   Password : z.string().min(2)
 });
 
